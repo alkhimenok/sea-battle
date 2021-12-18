fe263c2c8e9290b52dbccb4241a3d051e1c1af3d
Kirill Alkhimenok <alkhimenok.kirill.leonidovich@gmail.com>
2021-12-18 04:58:39 +0300

    add: size

--- routes/game.js
+++ routes/game.js
@@ -2,6 +2,8 @@ const { Router } = require('express')
 
 const router = Router()
 
-router.get('/', (req, res) => res.render('game'))
+router.get('/', (req, res) => res.render('game', { progress: true }))
+
+// router.put('/', (req, res) => res.render('game', { progress: true })) // change saving game // localstorage
 
 module.exports = router
