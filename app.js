1811bedf1880f7c98cedb4bc536b1c1aa5a84c70
Kirill Alkhimenok <alkhimenok.kirill.leonidovich@gmail.com>
2021-12-28 00:28:25 +0300

    fix: express name

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const path = require('path')
-const exporess = require('express')
+const express = require('express')
 
-const app = exporess()
+const app = express()
 
 const PORT = process.env.PORT || 5000
 
 app.set('view engine', 'ejs')
 app.set('views', path.resolve(__dirname, 'templates'))
 
-app.use(exporess.static(path.resolve(__dirname, 'client', 'dist')))
+app.use(express.static(path.resolve(__dirname, 'client', 'dist')))
 
 app.use('/', require('./routes/game'))
 
