264bcdf4366ef2ea81b84c50c7409a35c0c2b017
Kirill Alkhimenok <alkhimenok.kirill.leonidovich@gmail.com>
2022-01-07 20:49:04 +0300

    refactor: murkup

--- client/src/scripts/stats.js
+++
@@ -1,7 +0,0 @@
-import { getItemFromDB } from './dataBase'
-
-const statsValueList = document.querySelectorAll('[data-input="statsValue"]')
-
-export const setStats = () => {
-	statsValueList.forEach($input => ($input.value = getItemFromDB($input.id) || 0))
-}
--- routes/game.js
+++ routes/game.js
@@ -4,11 +4,4 @@ const router = Router()
 
 router.get('/', (req, res) => res.render('game', { progress: true }))
 
-// router.put('/', (req, res) => {
-//   res.render('game', { progress: true })
-
-// })
- // change saving game // localstorage
-// router.put('/', (req, res) => res.render('game', { progress: true })) // stats
-
 module.exports = router
